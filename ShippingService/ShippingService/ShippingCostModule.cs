764a966aefe17506eedd255c96f1d29450e8f369
Ryan Marcotte <RyanM@iqmetrix.com>
2015-01-14 13:08:02 -0600

    Added shells for shipping modules

---
+++ ShippingService/ShippingService/ShippingCostModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShippingService
+{
+	public class ShippingCostModule : NancyModule
+	{
+		public ShippingCostModule()
+		{
+			Get["/"] = x => GetShippingCost(x.PostalCode, x.TotalSize, x.TotalWeight);
+		}
+
+		public object GetShippingCost(string postalCode, decimal totalSize, decimal totalWeight)
+		{
+			return 10.0m;
+		}
+	}
+}
---
+++ ShippingService/ShippingService/ShippingLabelModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShippingService
+{
+	public class ShippingLabelModule : NancyModule
+	{
+		public ShippingLabelModule()
+		{
+			Get["/"] = x => GetShippingLabel();
+		}
+
+		public object GetShippingLabel()
+		{
+			return Guid.NewGuid();
+		}
+	}
+}
