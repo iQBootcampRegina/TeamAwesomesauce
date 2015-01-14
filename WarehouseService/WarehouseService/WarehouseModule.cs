55c6b8c14c25560c599112871ee139e18a86a85c
Kael Dow <kaeld@iqmetrix.com>
2015-01-14 12:46:08 -0600

    first WarehouseService commit

---
+++ WarehouseService/WarehouseService/WarehouseModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace WarehouseService
+{
+	public class WarehouseModule : NancyModule
+	{
+		public WarehouseModule()
+		{
+			Get["/Product/{id}/Location"] = x => GetLocationForProduct(x.id);
+		}
+
+		private object GetLocationForProduct(Guid productID)
+		{
+			return new
+				{
+					Row = new Random().Next(1000),
+					Shelf = new Random().Next(1000),
+				};
+
+		}
+	}
+}
