7dc7a411a8386e813eebdd2d06cb7a608f63f43e
LanceErickson <lancee@iqmetrix.com>
2015-01-15 14:46:33 -0600

    Stuff that makes the stuff work

--- PaymentService/PaymentService/Messages/SubmitPaymentMessage.cs
+++ PaymentService/PaymentService/Messages/SubmitPaymentMessage.cs
@@ -9,6 +9,11 @@ namespace PaymentService.Messages
 {
 	class SubmitPaymentMessage : ISubmitPaymentMessage
 	{
+		public SubmitPaymentMessage()
+		{
+			
+		}
+		
 		public SubmitPaymentMessage(Guid cartID, string billingInfo, decimal amount)
 		{
 			this.CartID = cartID;
